b6e7f51a41f410a1685dae7223b63bc2672c5e85
Christian Carter <christian@biggerboatconsulting.com>
2017-02-17 14:04:38 -0800

    lint and doc util.py

--- cumulusci/tasks/util.py
+++ cumulusci/tasks/util.py
@@ -1,3 +1,4 @@
+""" Utility tasks for use in CumulusCI Flows """
 import os
 import time
 
@@ -5,6 +6,9 @@ from cumulusci.core.tasks import BaseTask
 from cumulusci.utils import download_extract_zip
 
 class DownloadZip(BaseTask):
+    """ Download a zip file from a URL and extract it to the filesystem.
+    """
+
     name = 'Download'
     task_options = {
         'url': {
@@ -16,31 +20,34 @@ class DownloadZip(BaseTask):
             'required': True,
         },
         'subfolder': {
-            'description': 'The subfolder of the target zip to extract.  Defaults to extracting the root of the zip file to the destination.',
+            'description': 'The subfolder of the target zip to extract. ' \
+            'Defaults to extracting the root of the zip file to the destination.',
         },
     }
-   
+
     def _run_task(self):
         if not self.options['dir']:
             self.options['dir'] = '.'
         elif not os.path.exists(self.options['dir']):
             os.makedirs(self.options['dir'])
-            
-        download_extract_zip(self.options['url'], self.options['dir'], self.options.get('subfolder'))
-         
-    
+
+        download_extract_zip(
+            self.options['url'],
+            self.options['dir'],
+            self.options.get('subfolder')
+        )
 
 class Sleep(BaseTask):
+    """ Wait for the given number of seconds before succeeding. """
     name = 'Sleep'
     task_options = {
         'seconds': {
             'description': 'The number of seconds to sleep',
             'required': True,
         },
-    } 
-    
+    }
+
     def _run_task(self):
-        self.logger.info("Sleeping for {} seconds".format(self.options['seconds']))
+        self.logger.info("Sleeping for %(time)s seconds", self.options['seconds'])
         time.sleep(self.options['seconds'])
         self.logger.info("Done")
-
